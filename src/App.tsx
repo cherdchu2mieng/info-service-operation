1538e254df2860c8001fcfd301ecf93eddbbbc9c
cherdchu2mieng <cherdchu2mieng@gmail.com>
2026-05-03 09:20:40 +0700

    fix: adjust flow logic and improve line visibility 🦜

--- src/App.tsx
+++ src/App.tsx
@@ -36,36 +36,50 @@ function App() {
       </header>
 
       <div className='workflow-canvas'>
-        <svg className='svg-overlay' viewBox='0 0 1000 1100' preserveAspectRatio='none'>
-          <path d='M 500 40 L 500 120' className='flow-path-animated' />
-          <path d='M 500 220 L 500 280' className='flow-path-animated' />
-          <path d='M 500 280 L 250 280 L 250 360' className='flow-path-animated' />
-          <path d='M 500 280 L 500 360' className='flow-path-animated' />
-          <path d='M 500 280 L 750 280 L 750 360' className='flow-path-animated' />
+        <svg className='svg-overlay' viewBox='0 0 1000 1200' preserveAspectRatio='none'>
+          {/* SD to OC */}
+          <path d='M 500 50 L 500 120' className='flow-path-animated' />
+          
+          {/* OC to Boards */}
+          <path d='M 500 240 L 500 300' className='flow-path-animated' />
+          <path d='M 500 300 L 250 300 L 250 360' className='flow-path-animated' />
+          <path d='M 500 300 L 500 360' className='flow-path-animated' />
+          <path d='M 500 300 L 750 300 L 750 360' className='flow-path-animated' />
+          
+          {/* Boards to Staff Operation */}
           <path d='M 250 510 L 250 560 L 500 560 L 500 620' className='flow-path-animated' />
           <path d='M 500 510 L 500 620' className='flow-path-animated' />
           <path d='M 750 510 L 750 560 L 500 560 L 500 620' className='flow-path-animated' />
+          
+          {/* Staff Operation to Staff Closure */}
           <path d='M 500 770 L 500 840' className='flow-path-animated' />
-          <path d='M 500 960 L 500 1030' className='flow-path-animated' />
+          
+          {/* Staff Closure to Knowledge */}
+          <path d='M 500 960 L 500 1050' className='flow-path-animated' />
         </svg>
 
         <div className='node-row'>
           <Node id='sd' icon={<Headphones size={22} />} title='Service Desk' desc='รับเรื่องแจ้งปัญหา' className='node-service-desk' />
         </div>
+        
         <div className='node-row'>
           <Node id='oc' icon={<Settings size={22} />} title='Ops Control' desc='วิเคราะห์และจ่ายงาน' className='node-ops-control' />
         </div>
+        
         <div className='node-row'>
           <Node id='ba' icon={<Layout size={18} />} title='Board: App' desc='สายงานซอฟต์แวร์' className='node-board-app' />
           <Node id='bt' icon={<Cpu size={18} />} title='Board: Tech' desc='สายงานโครงสร้าง' className='node-board-tech' />
           <Node id='bf' icon={<Package size={18} />} title='Board: Fulfill' desc='สายงานจัดหา' className='node-board-fulfill' />
         </div>
+        
         <div className='node-row'>
           <Node id='staff-op' icon={<Users size={22} />} title='Staff Operation' desc='เจ้าหน้าที่ดำเนินงาน' className='node-staff-op' />
         </div>
+        
         <div className='node-row'>
           <Node id='staff-cl' icon={<UserCheck size={22} />} title='Staff Closure' desc='เจ้าหน้าที่ปิดจบงาน' className='node-staff' />
         </div>
+        
         <div className='node-row'>
           <Node id='kb' icon={<LibraryBig size={22} />} title='Knowledge' desc='บันทึกฐานความรู้' className='node-kb' />
         </div>
